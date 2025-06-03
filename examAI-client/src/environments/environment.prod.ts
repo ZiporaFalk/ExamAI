e9c3acff86d7664d102e9d3f4d39632d3c0df4b1
ZiporaFalk <z0548498935@gmail.com>
2025-06-03 16:08:18 +0300

    environmentAngular

---
+++ examAI-client/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    apiUrl: 'https://examaiserver.onrender.com/api',
+    googleClientId: '980760489243-eo8i966l8siignphbpn1u33tanlcus0g.apps.googleusercontent.com'
+};
--- examAI-client/src/interceptors/auth.interceptor.ts
+++ examAI-client/src/interceptors/auth.interceptor.ts
@@ -5,8 +5,6 @@ import { environment } from '../environments/environment';
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('token');
 
-  // const isRelative = !/^http/.test(req.url);
-  // const fullUrl = isRelative ? API_BASE_URL + req.url : req.url;
   const apiUrl = req.url.startsWith('http') ? req.url : `${environment.apiUrl}${req.url}`;
   console.log(apiUrl);
   console.log("apiUrl");
@@ -20,20 +18,3 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
   }
   return next(req);
 }
-
-///////////////
-// import { HttpInterceptorFn } from '@angular/common/http';
-// import { environment } from '../environments/environment';
-// export const TokenInterceptor: HttpInterceptorFn = (req, next) => {
-//   const token = localStorage.getItem('token');
-
-//   // אם ה-URL לא מלא (כלומר לא מתחיל ב-http), נוסיף את baseUrl מהסביבה
-//   const apiUrl = req.url.startsWith('http') ? req.url : `${environment.apiUrl}${req.url}`;
-//   const clonedReq = req.clone({
-//     url: apiUrl,
-//     setHeaders: token ? {
-//       Authorization: `Bearer ${token}`
-//     } : {}
-//   });
-//   return next(clonedReq);
-// };
