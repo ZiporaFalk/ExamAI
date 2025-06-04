80d87c5f457271aa570fe689c792df561620b75c
ZiporaFalk <z0548498935@gmail.com>
2025-06-04 13:16:32 +0300

    prodAngular

--- examAI-client/src/environments/environment.ts
+++ examAI-client/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
-    production: false,
     apiUrl: 'https://localhost:7083/api',
     googleClientId: '980760489243-eo8i966l8siignphbpn1u33tanlcus0g.apps.googleusercontent.com'
 } 
